27e2e43f60a75f0316c21c7dbb8a44072f4dd207
Kokkinis Efstratios <efkokkinis@gmail.com>
2016-12-02 20:38:25 +0100

    Create PdfTextExtraction.py

---
+++ PdfTextExtraction.py
@@ -0,0 +1,18 @@
+import pyPdf
+
+def getPDFContent(path):
+    content = ""
+    # Load PDF into pyPDF
+    pdf = pyPdf.PdfFileReader(file(path, "rb"))
+    # Iterate pages
+    for i in range(0, pdf.getNumPages()):
+        # Extract text from page and add to content
+        content += pdf.getPage(i).extractText() + "\n"
+    # Collapse whitespace
+    content = " ".join(content.replace(u"\xa0", " ").strip().split())
+    return content
+
+
+
+
+print getPDFContent("g.pdf").encode("ascii", "ignore")
