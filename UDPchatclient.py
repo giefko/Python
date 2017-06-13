d07fca6bfcdbc9b4cc986ec5c28b51572dcd1f1b
Kokkinis Efstratios <efkokkinis@gmail.com>
2017-06-13 21:50:13 +0200

    Add files via upload

---
+++ UDPchatclient.py
@@ -0,0 +1,16 @@
+import os
+from socket import *
+host = "localhost" # set to IP address of target computer
+port = 21561
+addr = (host, port)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    data = raw_input("Enter message to send or type 'exit': ")
+    UDPSock.sendto(data, addr)
+    if data == "exit":
+        break
+UDPSock.close()
+os._exit(0)
+
+
+
