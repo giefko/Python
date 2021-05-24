d3d4dd7e9481e7008a6878fb15fc56fb272f8635
Kokkinis Efstratios <efkokkinis@gmail.com>
2021-05-24 23:18:49 +0200

    Add files via upload
    You can add any python script on Startup in windows 10 environment

---
+++ Python 3.8/WxPython/Windows/autorun.py
@@ -0,0 +1,38 @@
+import winreg as reg
+import os
+
+from winreg import CreateKey, OpenKey, ConnectRegistry
+
+
+def AddToRegistry():
+    # in python __file__ is the instant of
+    # file path where it was executed
+    # so if it was executed from desktop,
+    # then __file__ will be
+    # c:\users\current_user\desktop
+    pth = os.path.dirname(os.path.realpath(__file__))
+
+    # name of the python file with extension
+    s_name = "path.to your.py"
+
+    # joins the file name to end of path address
+    address = os.path.join(pth, s_name)
+
+    # key we want to change is HKEY_CURRENT_USER
+    # key value is Software\Microsoft\Windows\CurrentVersion\Run
+    key = reg.HKEY_CURRENT_USER
+    key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
+
+    # open the key to make changes to
+    open = reg.OpenKey(key, key_value, 0, reg.KEY_ALL_ACCESS)
+
+    # modifiy the opened key
+    reg.SetValueEx(open, "any_name", 0, reg.REG_SZ, address)
+
+    # now close the opened key
+    reg.CloseKey(open)
+
+
+# Driver Code
+if __name__ == "__main__":
+    AddToRegistry()
