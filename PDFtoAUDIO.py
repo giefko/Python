48139db995d33dc277fd95933ca144279904b1e6
Kokkinis Efstratios <efkokkinis@gmail.com>
2023-04-03 18:18:07 +0200

    Add files via upload
    Simple PDF to Audio

---
+++ PDFtoAUDIO.py
@@ -0,0 +1,19 @@
+import PyPDF3
+import pyttsx3
+import pdfplumber
+
+file = input("Add the name of your pdf file")
+book = open(file, 'rb')
+pdfReader = PyPDF3.PdfFileReader(book)
+pages = pdfReader.numPages
+finalText = ""
+
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        finalText += text
+
+engine = pyttsx3.init()
+engine.save_to_file(finalText, 'YourNewAudio.mp3')
+engine.runAndWait()
